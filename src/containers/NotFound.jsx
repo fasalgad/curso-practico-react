e80bc8e8d8c327b02fbfa5fb16b7afc2d49ade51
Fabian Salgado <Fabian Salgado>
2020-07-30 02:56:48 -0400

    clase 6

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable react/button-has-type */
+/* eslint-disable indent */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable no-tabs */
+/* eslint-disable react/jsx-indent */
+import React from 'react';
+
+const NotFound = () => (
+	<>
+		<h1>No encontrado</h1>
+		<h2>Regresa al Home</h2>
+	</>
+);
+
+export default NotFound;
