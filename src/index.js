121032c3a0d835fe4d15d93677323b51bfd5ece2
Fabian Salgado <Fabian Salgado>
2020-07-30 03:25:22 -0400

    clase 11 instalación de redux y react-redux y modificación de index.js del SRC

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
+/* eslint-disable react/jsx-indent */
+/* eslint-disable no-tabs */
+/* eslint-disable indent */
 import React from 'react';
 import ReactROM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './routes/App';
 
-ReactROM.render(<App />, document.getElementById('app'));
+ReactROM.render(
+	<Provider>
+		<App />
+	</Provider>,
+	document.getElementById('app'),
+);
