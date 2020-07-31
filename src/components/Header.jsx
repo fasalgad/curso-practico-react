e7fc292d28f3156fa2ffedc66275a73efd111d7a
Fabian Salgado <Fabian Salgado>
2020-07-31 02:13:55 -0400

    clase 19,20 login y logout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,20 @@
 /* eslint-disable react/jsx-indent */
 /* eslint-disable indent */
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import gravatar from '../utils/gravatar';
+import { logoutRequest } from '../actions';
 import '../assets/styles/components/Header.scss';
 import logo from '../assets/static/logo-platzi-video-BW2.png';
 import userIcon from '../assets/static/user-icon.png';
 
-const Header = () => {
+const Header = (props) => {
+	const { user } = props;
+	const hasUser = Object.keys(user).length > 0;
+	const handleLogout = () => {
+		props.logoutRequest({});
+	};
 	return (
 		<header className='header'>
 			<Link to='/'>
@@ -16,20 +24,43 @@ const Header = () => {
 			</Link>
 			<div className='header__menu'>
 				<div className='header__menu--profile'>
-					<img src={userIcon} alt='' />
+					{hasUser ? (
+						<img src={gravatar(user.email)} alt={user.email} />
+					) : (
+						<img src={userIcon} alt='user icon' />
+					)}
 					<p>Perfil</p>
 				</div>
 				<ul>
-					<li>
-						<a href='/'>Cuenta</a>
-					</li>
-					<li>
-						<Link to='/login'>Iniciar sesión</Link>
-					</li>
+					{hasUser ? (
+						<li>
+							<a href='/'>{user.name}</a>
+						</li>
+					) : null}
+					{hasUser ? (
+						<li>
+							<Link to='#logout' onClick={handleLogout}>
+								Cerrar sesión
+							</Link>
+						</li>
+					) : (
+						<li>
+							<Link to='/login'>Iniciar sesión</Link>
+						</li>
+					)}
 				</ul>
 			</div>
 		</header>
 	);
 };
 
-export default Header;
+const mapStateToProps = (state) => {
+	return {
+		user: state.user,
+	};
+};
+const mapDispatchToProps = {
+	logoutRequest,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
--- src/utils/gravatar.js
+++ src/utils/gravatar.js
@@ -5,7 +5,8 @@ import md5 from 'md5';
 
 const gravatar = (email) => {
 	const base = 'https://gravatar.com/avatar/';
-	const formatEmail = email.trim().toLoweCase();
+	console.log(email)
+	const formatEmail = (email).trim().toLowerCase();
 	const hash = md5(formatEmail, { encoding: 'binary' });
 	return `${base}${hash}`;
 };
