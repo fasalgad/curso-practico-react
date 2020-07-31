310278da8a139fb09cb20d3b2f2f4c3a133ddcef
Fabian Salgado <Fabian Salgado>
2020-07-31 19:30:59 -0400

    clases 24 y 25

--- src/containers/Player.jsx
+++ src/containers/Player.jsx
@@ -1,20 +1,47 @@
+/* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/no-unknown-property */
 /* eslint-disable no-tabs */
 /* eslint-disable react/jsx-indent */
 /* eslint-disable indent */
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
+import { connect } from 'react-redux';
 
-const Player = () => {
-	return (
+import '../assets/styles/components/Player.scss';
+// import { Redirect } from 'react-router-dom';
+import { getVideoSource } from '../actions';
+import NotFound from './NotFound';
+
+const Player = (props) => {
+	const { id } = props.match.params;
+	const hashPlaying = Object.keys(props.playing).length > 0;
+
+	useLayoutEffect(() => {
+		props.getVideoSource(id);
+	}, []);
+
+	return hashPlaying ? (
 		<div className='Player'>
 			<video controls autoPlay>
-				<source src='' type='video/mp4' />
+				<source src={props.playing.source} type='video/mp4' />
 			</video>
-			<div className='Player-black'>
-				<button type='button'>Regresar</button>
+			<div className='Player-back'>
+				<button type='button' onClick={() => props.history.goBack()}>
+					Regresar
+				</button>
 			</div>
 		</div>
+	) : (
+		<NotFound />
 	);
 };
 
-export default Player;
+const mapStateToProps = (state) => {
+	return {
+		playing: state.playing,
+	};
+};
+const mapDispatchToProps = {
+	getVideoSource,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Player);
